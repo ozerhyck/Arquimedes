07e4f5d7efdf8a95428ceee1c7ca9cbc14b2d219
Marcio Henrique <marciorique@hotmail.com>
2018-03-22 12:44:06 -0300

    Up do projeto no estado alpha

--- src/Arquimedes/Program.cs
+++ src/Arquimedes/Program.cs
@@ -12,9 +12,6 @@ namespace Arquimedes
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
